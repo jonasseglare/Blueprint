8be3d926d04800ad797ecf959aedcd839d1bf894
Jonas Östlund <uppfinnarjonas@gmail.com>
2022-07-27 15:24:48 +0200

    wip

--- designs/insulation_board.jl
+++ designs/insulation_board.jl
@@ -1,4 +1,25 @@
-using Blueprint
+import Blueprint
+const bp = Blueprint
 
-function insulation_board()
+const length = 2.30
+const inner_overall_width = 1.25
+const inner_board_count = 3
+
+const inner_board_raw_width = inner_overall_width/inner_board_count
+
+const left_wall = bp.plane_at_pos([1.0, 0.0, 0.0], [0.0, 0.0, 0.0])
+const right_wall = bp.plane_at_pos([-1.0, 0.0, 0.0], [length, 0.0, 0.0])
+
+const strong_beam = bp.beam_specs(3.0, 4.0)
+
+function inner_insulation_board(offset)
+    
 end
+
+function render()
+    x = inner_insulation_board(0)
+    #inner_boards = bp.group(inner_insulation_board(0))
+    #return bp.group(inner_boards)
+end
+
+render()
